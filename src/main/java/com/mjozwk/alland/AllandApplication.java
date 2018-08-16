9932e801c75ddc77c280b94e25aab48c20615742
marcin.jozwiak <mjozwk@gmail.com>
2018-08-16 10:58:31 +0200

    init commit

---
+++ src/main/java/com/mjozwk/alland/AllandApplication.java
@@ -0,0 +1,12 @@
+package com.mjozwk.alland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllandApplication.class, args);
+	}
+}
